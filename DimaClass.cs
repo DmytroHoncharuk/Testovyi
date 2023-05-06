b8c136e6e5e64172b490c631e96c0727b52e921b
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2023-05-06 19:44:42 +0300

    Створив новий клас "Dima"

---
+++ DimaClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testoveGit
+{
+    internal class DimaClass
+    {
+        //цей клас має бути доступний лише в гілці "DIMA"
+    }
+}
